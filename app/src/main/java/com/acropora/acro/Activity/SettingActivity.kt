9dbd54ae794d08fda8f8c788e92f25477609231c
KemasMYunus <kemasmuhammadyunus@gmail.com>
2024-02-26 18:00:01 +0800

    Melanjutkan Halaman Setting #2

--- app/src/main/java/com/acropora/acro/Activity/SettingActivity.kt
+++ app/src/main/java/com/acropora/acro/Activity/SettingActivity.kt
@@ -1,12 +1,25 @@
 package com.acropora.acro.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.acropora.acro.R
+import com.acropora.acro.databinding.ActivityMainBinding
+import com.acropora.acro.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            tombolLogout.setOnClickListener{
+                startActivity(Intent(this@SettingActivity,LoginAcivity::class.java))
+                overridePendingTransition(0, 0) // Menghilangkan animasi
+                finish()
+            }
+        }
     }
 }
