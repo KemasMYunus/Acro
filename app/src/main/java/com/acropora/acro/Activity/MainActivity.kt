de3f0e292dc81e045dc0b24a1092d2a1ed61f41f
KemasMYunus <kemasmuhammadyunus@gmail.com>
2024-02-27 22:22:34 +0800

    Mengubah halaman home

--- app/src/main/java/com/acropora/acro/Activity/MainActivity.kt
+++ app/src/main/java/com/acropora/acro/Activity/MainActivity.kt
@@ -13,9 +13,20 @@ import com.acropora.acro.databinding.ActivityMainBinding
 import com.acropora.acro.Model.StandingModel
 import com.acropora.acro.Adapter.StandingTableViewAdapter
 import android.os.Handler
+import android.os.Looper
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.acropora.acro.Adapter.ImageAdapter
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var  viewPager2: ViewPager2
+    private lateinit var handler : Handler
+    private lateinit var imageList:ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -54,6 +65,16 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+        init()
+        setUpTransformer()
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable , 2000)
+            }
+        })
 
         val recyclerViewMovieList = findViewById<RecyclerView>(R.id.recyclerViewMovieList)
         recyclerViewMovieList.layoutManager = LinearLayoutManager(this)
@@ -71,6 +92,58 @@ class MainActivity : AppCompatActivity() {
         add(StandingModel(6, "Titan Sharks", 3, 1, 2, 4))
         add(StandingModel(7, "Citra Satria", 3, 0, 3, 3))
         add(StandingModel(8, "Triple One", 3, 0, 3, 3))
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable , 2000)
+    }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer(){
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init(){
+        viewPager2 = findViewById(R.id.viewPager2)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.team1)
+        imageList.add(R.drawable.team2)
+        imageList.add(R.drawable.team3)
+        imageList.add(R.drawable.team4)
+        imageList.add(R.drawable.team5)
+        imageList.add(R.drawable.team6)
+        imageList.add(R.drawable.team7)
+        imageList.add(R.drawable.team8)
+
+
+        adapter = ImageAdapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
 
     }
 
